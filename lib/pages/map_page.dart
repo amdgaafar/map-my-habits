da9cc65db7a19c2ddc9d078a6d5b7f856d09a02e
amdgaafar <amdgaafar@gmail.com>
2024-01-10 20:09:07 +0900

    create layout for map page 2

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_heatmap_calendar/simple_heatmap_calendar.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key, required this.title}) : super(key: key);
@@ -10,12 +11,58 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPage extends State<MapPage> {
+  final Map<DateTime, num> testSimple = {
+    DateTime(2024, 1, 1): 40,
+    DateTime(2024, 1, 2): 40,
+    DateTime(2024, 1, 3): 40,
+    DateTime(2024, 1, 8): 40,
+  };
 
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title), centerTitle: true,),
-      body: Container(),
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            color: Colors.white.withOpacity(0.9),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: HeatmapCalendar<num>(
+                startDate: DateTime(2023, 12, 1),
+                endedDate: DateTime(2024, 1, 9),
+                firstDay: DateTime.monday,
+                colorMap: {
+                  10: theme.primaryColor.withOpacity(0.2),
+                  20: theme.primaryColor.withOpacity(0.4),
+                  30: theme.primaryColor.withOpacity(0.6),
+                  40: theme.primaryColor.withOpacity(0.8),
+                  50: theme.primaryColor,
+                },
+                selectedMap: testSimple,
+                cellSize: const Size.square(26),
+                colorTipCellSize: const Size.square(12.0),
+                style: const HeatmapCalendarStyle.defaults(
+                  cellValueFontSize: 6.0,
+                  cellRadius: BorderRadius.all(Radius.circular(4.0)),
+                  weekLabelValueFontSize: 12.0,
+                  monthLabelFontSize: 12.0,
+                ),
+                layoutParameters: const HeatmapLayoutParameters.defaults(
+                  monthLabelPosition: CalendarMonthLabelPosition.top,
+                  weekLabelPosition: CalendarWeekLabelPosition.right,
+                  colorTipPosition: CalendarColorTipPosition.bottom,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
