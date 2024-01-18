f18aa81e9e4b2a8700007dc213b62287bcd0ee4b
amdgaafar <amdgaafar@gmail.com>
2024-01-18 16:29:24 +0900

    Add habit records to heatmap calendar

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:map_my_habits/models/habit.dart';
+import 'package:map_my_habits/objectbox.g.dart';
 import 'package:simple_heatmap_calendar/simple_heatmap_calendar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key, required this.title}) : super(key: key);
@@ -11,12 +15,40 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPage extends State<MapPage> {
-  final Map<DateTime, num> testSimple = {
-    DateTime(2024, 1, 1): 40,
-    DateTime(2024, 1, 2): 40,
-    DateTime(2024, 1, 3): 40,
-    DateTime(2024, 1, 8): 40,
-  };
+  late Store _store;
+
+  Map<DateTime, num> daysChecked = {};
+
+  @override
+  void initState() {
+    super.initState();
+    getApplicationDocumentsDirectory().then((dir) {
+      _store = Store(
+        getObjectBoxModel(),
+        directory: join(dir.path, 'objectbox'),
+      );
+      _fetchHabitRecords();
+    });
+  }
+
+  @override
+  void dispose() {
+    _store.close();
+    super.dispose();
+  }
+
+  Future<void> _fetchHabitRecords() async {
+    final box = _store.box<HabitRecord>();
+    final records = box.getAll();
+    setState(() {
+      daysChecked.addAll({
+        for (var record in records)
+          DateTime(record.date.year, record.date.month, record.date.day):
+              record.isDone ? 30 : 0,
+      });
+    });
+    print(daysChecked);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,16 +67,16 @@ class _MapPage extends State<MapPage> {
               padding: const EdgeInsets.all(10.0),
               child: HeatmapCalendar<num>(
                 startDate: DateTime(2023, 12, 1),
-                endedDate: DateTime(2024, 1, 9),
+                endedDate: DateTime(2024, 1, 31),
                 firstDay: DateTime.monday,
                 colorMap: {
-                  10: theme.primaryColor.withOpacity(0.2),
-                  20: theme.primaryColor.withOpacity(0.4),
-                  30: theme.primaryColor.withOpacity(0.6),
-                  40: theme.primaryColor.withOpacity(0.8),
-                  50: theme.primaryColor,
+                  0: theme.primaryColor.withOpacity(0.2),
+                  1: theme.primaryColor.withOpacity(0.4),
+                  2: theme.primaryColor.withOpacity(0.6),
+                  3: theme.primaryColor.withOpacity(0.8),
+                  4: theme.primaryColor,
                 },
-                selectedMap: testSimple,
+                selectedMap: daysChecked,
                 cellSize: const Size.square(26),
                 colorTipCellSize: const Size.square(12.0),
                 style: const HeatmapCalendarStyle.defaults(
